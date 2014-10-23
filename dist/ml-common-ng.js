27e30e4029a7759d2c50f217aa0ed54cd69b1275
Joseph Bryan <joemfb@gmail.com>
2014-10-23 15:52:22 -0400

    v0.0.3

--- dist/ml-common-ng.js
+++ dist/ml-common-ng.js
@@ -1,7 +1,159 @@
+(function() {
+  'use strict';
+
+  angular.module('ml.common', []);
+
+}());
+
+(function() {
+  'use strict';
+
+  angular.module('ml.common')
+    .factory('MLQueryBuilder', MLQueryBuilder);
+
+  function MLQueryBuilder() {
+    return {
+
+      query: function query() {
+        var args = asArray.apply(null, arguments);
+        return {
+          'query': {
+            'queries': args
+          }
+        };
+      },
+
+      text: function text(qtext) {
+        return {
+          'qtext': qtext
+        };
+      },
+
+      and: function and() {
+        var args = asArray.apply(null, arguments);
+        return {
+          'and-query': {
+            'queries': args
+          }
+        };
+      },
+
+      or: function or() {
+        var args = asArray.apply(null, arguments);
+        return {
+          'or-query': {
+            'queries': args
+          }
+        };
+      },
+
+      not: function properties(query) {
+        return {
+          'not-query': query
+        };
+      },
+
+      document: function document() {
+        var args = asArray.apply(null, arguments);
+        return {
+          'document-query': {
+            'uri': args
+          }
+        };
+      },
+
+      range: function range(name, values) {
+        values = asArray.apply(null, [values]);
+        return {
+          'range-constraint-query': {
+            'constraint-name': name,
+            'value': values
+          }
+        };
+      },
+
+      collection: function collection(name, values) {
+        values = asArray.apply(null, [values]);
+        return {
+          'collection-constraint-query': {
+            'constraint-name': name,
+            'uri': values
+          }
+        };
+      },
+
+      custom: function custom(name, values) {
+        values = asArray.apply(null, [values]);
+        return {
+          'custom-constraint-query': {
+            'constraint-name': name,
+            'value': values
+          }
+        };
+      },
+
+      constraint: function constraint(type) {
+        switch(type) {
+          case 'custom':
+            return this.custom;
+          case 'collection':
+            return this.collection;
+          default:
+            return this.range;
+        }
+      },
+
+      boost: function boost(matching, boosting) {
+        return {
+          'boost-query': {
+            'matching-query': matching,
+            'boosting-query': boosting
+          }
+        };
+      },
+
+      properties: function properties(query) {
+        return { 'properties-query': query };
+      },
+
+      operator: function operator(name, stateName) {
+        return {
+          'operator-state': {
+            'operator-name': name,
+            'state-name': stateName
+          }
+        };
+      }
+
+    };
+
+  }
+
+  function asArray() {
+    var args;
+
+    if ( arguments.length === 0 ) {
+      args = [];
+    } else if ( arguments.length === 1) {
+      if (Array.isArray( arguments[0] )) {
+        args = arguments[0];
+      } else {
+        args = [ arguments[0] ];
+      }
+    } else {
+      args = [].slice.call(arguments);
+    }
+
+    return args;
+  }
+
+
+}());
+
 (function () {
   'use strict';
 
-  angular.module('ml.common', [])
+  angular.module('ml.common')
     .provider('MLRest', function() {
         this.$get = ['$q', '$http', MLRest];
     });
@@ -32,7 +184,7 @@
 
       // DEPRECATED: TODO: remove
       getSearchOptions: queryConfig,
-      callExtension: request,
+      callExtension: extension,
       patch: patchDocument
     };
 
@@ -201,7 +353,7 @@
     function queryConfig(name, section) {
       var url = '/config/query/' + name;
 
-      if (section && section.length) {
+      if (section) {
         url += '/' + section;
       }
       return request(url, {
--- src/ml-common.js
+++ src/ml-common.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
 
-  angular.module('ml.common', [])
+  angular.module('ml.common', []);
 
 }());
