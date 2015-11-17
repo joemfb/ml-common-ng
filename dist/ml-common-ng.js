3dab8fbbd3aab450c2b63aac5cdf581971e1bec1
Joseph Bryan <joemfb@gmail.com>
2015-11-17 16:43:45 -0500

    v1.0.1

--- dist/ml-common-ng.js
+++ dist/ml-common-ng.js
@@ -72,7 +72,11 @@
 
   // Copied from https://docs.angularjs.org/api/ng/service/$compile
   angular.module('ml.common')
-  .directive('compile', function($compile) {
+  .directive('compile', compile);
+
+  compile.$inject = ['$compile'];
+
+  function compile($compile) {
     // directive factory creates a link function
     return function(scope, element, attrs) {
       scope.$watch(
@@ -93,7 +97,7 @@
         }
       );
     };
-  });
+  }
 })();
 
 (function() {
